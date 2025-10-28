85ca124ee25bfcbd7d6a8a778d97e04c3e3bb80a
parktaejung <ppark5098@naver.com>
2025-10-29 00:22:39 +0900

    ci trigger2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 배우자 ㅎㅎ
+          Learn React 배우자 ㅎㅎfff
         </a>
       </header>
     </div>
